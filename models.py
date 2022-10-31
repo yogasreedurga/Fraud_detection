bbb48bf35f4a3460e3f0ea5de738bd6f8e2be5b0
yogasreedurga <sree2632@gmail.com>
2022-10-31 10:08:41 +0530

    Added 3 new ML model code

--- models.py
+++ models.py
@@ -1,1 +1,11 @@
 print("All models relating to Fraud detection techniques are listed here")
+
+def model1():
+    print("Adding code for ML model1")
+
+def model2():
+    print("Adding code for ML model2")
+
+def model3():
+    print("Adding code for ML model3")
+
